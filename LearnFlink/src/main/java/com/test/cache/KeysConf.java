d4d5fcd465e43644594f5fed5560bbd16dbfe2d3
13592513537@163.com <sjh13592513537githup**>
2018-12-18 17:36:25 +0800

    Merge branch 'master' of https://github.com/ReformSun/flink-master
    # Conflicts:
    #	LearnFlink/src/main/java/com/test/learnTableapi/TestMain5.java

---
+++ LearnFlink/src/main/java/com/test/cache/KeysConf.java
@@ -0,0 +1,9 @@
+package com.test.cache;
+
+public interface KeysConf {
+	public Key getFirstKey();
+	public Key getSecondKey();
+
+	public void setFirstKey(Key firstKey);
+	public void setSecondKey(Key secondKey);
+}
